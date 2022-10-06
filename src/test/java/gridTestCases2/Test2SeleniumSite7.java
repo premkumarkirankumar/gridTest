ea2a9535ff078f6fc02837699885dcd019445a96
premkumarkirankumar <35238556+premkumarkirankumar@users.noreply.github.com>
2022-10-06 13:51:25 -0400

    removed retry logic

--- src/test/java/gridTestCases2/Test2SeleniumSite7.java
+++ src/test/java/gridTestCases2/Test2SeleniumSite7.java
@@ -13,22 +13,22 @@ import org.testng.annotations.Test;
 
 import io.qameta.allure.Description;
 import utils.Base;
-import utils.RetryCommand;
 
-public class Test2SeleniumSite7   {
+public class Test2SeleniumSite7 {
 	public static WebDriver driver;
-	//public String gridUrl="http://10.62.234.23:4444";
-	
+	// public String gridUrl="http://10.62.234.23:4444";
+
 	@Description("Test to verify HomePage")
 	@Parameters("gridUrl")
 	@Test
 	public void testSeleniumPage(String gridUrl) throws Exception {
 		ChromeOptions options = new ChromeOptions();
-		RetryCommand retryCommand = new RetryCommand(5, 60);
-		RemoteWebDriver driver = retryCommand.execute(() -> new RemoteWebDriver(new URL(gridUrl), options));
-		//driver = new RemoteWebDriver(new URL(gridUrl), options);
+		// RetryCommand retryCommand = new RetryCommand(5, 60);
+		// RemoteWebDriver driver = retryCommand.execute(() -> new RemoteWebDriver(new
+		// URL(gridUrl), options));
+		driver = new RemoteWebDriver(new URL(gridUrl), options);
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(300));
-		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(60));		
+		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(60));
 		driver.manage().timeouts().setScriptTimeout(Duration.ofSeconds(300));
 
 		System.setProperty("webdriver.chrome.silentOutput", "true");
@@ -36,14 +36,14 @@ public class Test2SeleniumSite7   {
 		System.out.println("Url loaded");
 		System.out.println("Intentional Wait");
 		Base.intentionalWaitValue();
-	    WebElement l =driver.findElement(By.tagName("body"));
-	    System.out.println("Intentional Wait");
-	    Base.intentionalWaitValue();
-	    System.out.println("Text content: "+ l.getText());
-	    driver.manage().window().maximize();
-	    System.out.println("Intentional Wait");
-	    Base.intentionalWaitValue();
-	    driver.quit();
+		WebElement l = driver.findElement(By.tagName("body"));
+		System.out.println("Intentional Wait");
+		Base.intentionalWaitValue();
+		System.out.println("Text content: " + l.getText());
+		driver.manage().window().maximize();
+		System.out.println("Intentional Wait");
+		Base.intentionalWaitValue();
+		driver.quit();
 	}
 
 }
