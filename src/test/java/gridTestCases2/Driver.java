dcd8aef9e199ef640f0fa56d9a0fa596336d85b6
Alex Siminiuc <alsiminiuc@teksystems.com>
2022-10-13 15:10:09 -0400

    change client side timeout to 10 mins

--- src/test/java/gridTestCases2/Driver.java
+++ src/test/java/gridTestCases2/Driver.java
@@ -2,15 +2,19 @@ package gridTestCases2;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.time.Duration;
 
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.remote.http.ClientConfig;
 
 public class Driver {
 	
 	public RemoteWebDriver createDriver(String gridUrl) throws MalformedURLException {
 		System.setProperty("webdriver.chrome.silentOutput", "true");
 
+		ClientConfig config = ClientConfig.defaultConfig().readTimeout(Duration.ofMinutes(10));
+		 
 		ChromeOptions options = new ChromeOptions();
 		options.setPlatformName("LINUX");
 		return new RemoteWebDriver(new URL(gridUrl), options);
