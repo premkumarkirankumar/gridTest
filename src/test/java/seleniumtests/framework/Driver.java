fd22eedd53dd998f2f6f15719b025379ef7a3d1b
Alex Siminiuc <alsiminiuc@teksystems.com>
2022-10-17 18:14:36 -0400

    increase driver timeout to 30 mins

--- src/test/java/seleniumtests/framework/Driver.java
+++ src/test/java/seleniumtests/framework/Driver.java
@@ -13,8 +13,8 @@ public class Driver {
 	public WebDriver createDriver(String gridUrl) throws MalformedURLException {
 		System.setProperty("webdriver.chrome.silentOutput", "true");
 
-		ClientConfig config = ClientConfig.defaultConfig().readTimeout(Duration.ofMinutes(10))
-														  .connectionTimeout(Duration.ofMinutes(10));
+		ClientConfig config = ClientConfig.defaultConfig().readTimeout(Duration.ofMinutes(30))
+														  .connectionTimeout(Duration.ofMinutes(30));
 		 
 		ChromeOptions options = new ChromeOptions();
 		options.setPlatformName("LINUX");
